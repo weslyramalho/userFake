7c38ebcebef1843d2c8ee594f204ec0313bf7a19
Wesly <weslyramalho@yahoo.com.br>
2022-03-26 17:08:50 -0300

    UserService

--- src/main/java/com/wr/userfake/UserService.java
+++ src/main/java/com/wr/userfake/UserService.java
@@ -1,5 +1,30 @@
 package com.wr.userfake;
 
-public class UserService {
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.persistence.MapsId;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+	
+	private static Map<Integer, String> USERS = new HashMap<>();
+
+	public UserService() {
+		USERS.put(1, "User 1");
+		USERS.put(2, "User 2");
+		USERS.put(3, "User 3");
+
+	}
+	
+	public String findUserById(final Integer id) {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return USERS.get(id);
+	}
 }
